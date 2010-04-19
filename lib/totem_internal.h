a7d91e7129517a99beb589f557ce5de04923641f
Shixin Zeng <zeng.shixin@gmail.com>
2010-04-19 17:22:09 -0500

    utilize the variadic macro support in msvc newer than VS8
    https://bugzilla.gnome.org/show_bug.cgi?id=575373

--- lib/totem_internal.h
+++ lib/totem_internal.h
@@ -7,7 +7,11 @@
 #ifdef __GNUC__
   #define asprintf(STRINGPP, FORMAT, ARGS...) totem_private_asprintf((STRINGPP), FORMAT, ##ARGS)
 #elif defined (_MSC_VER)
-  #define asprintf(STRINGPP, FORMATARGS) totem_private_asprintf((STRINGPP), FORMATARGS)
+	#if _MSC_VER >= 1400 /* Variadic macro is supported since VS 8 */
+		#define asprintf(STRINGPP, FORMAT, ...)  totem_private_asprintf(STRINGPP, FORMAT, __VA_ARGS__)
+	#else
+		#define asprintf(STRINGPP, FORMATARGS) totem_private_asprintf((STRINGPP), FORMATARGS)
+	#endif
 #else
   #define asprintf(STRINGPP, FORMAT, ...) totem_private_asprintf((STRINGPP), FORMAT, __VA_ARGS__)
 #endif
