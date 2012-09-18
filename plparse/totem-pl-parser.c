e2f4f8d73bd8f6bf6935046c0dd0530124c02486
Bastien Nocera <hadess@hadess.net>
2012-09-18 16:28:51 +0200

    plparse: Remove dep on newer versions of glib
    We should still work with older versions.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=684292

--- plparse/totem-pl-parser.c
+++ plparse/totem-pl-parser.c
@@ -1835,8 +1835,10 @@ totem_pl_parser_parse_internal (TotemPlParser *parser,
 
 	/* Not a directory on http though */
 	if (g_strcmp0 (mimetype, "inode/directory") == 0 &&
-	    g_file_has_uri_scheme (file, "http"))
-		g_clear_pointer (&mimetype, g_free);
+	    g_file_has_uri_scheme (file, "http")) {
+		g_free (mimetype);
+		mimetype = NULL;
+	}
 
 	DEBUG(file, g_print ("_get_mime_type_for_name for '%s' returned '%s'\n", uri, mimetype));
 	if (mimetype == NULL || strcmp (UNKNOWN_TYPE, mimetype) == 0
