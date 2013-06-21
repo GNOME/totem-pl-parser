24970018abfcedcaa4965e1f0f987aaf63323f31
Bastien Nocera <hadess@hadess.net>
2013-06-21 13:08:36 +0200

    plparse: Port to libquvi 0.9
    https://bugzilla.gnome.org/show_bug.cgi?id=700768

--- plparse/totem-pl-parser-videosite.c
+++ plparse/totem-pl-parser-videosite.c
@@ -24,46 +24,47 @@
 #include <string.h>
 #include <glib.h>
 
-#ifndef TOTEM_PL_PARSER_MINI
-#include "totem-disc.h"
-#endif /* !TOTEM_PL_PARSER_MINI */
-
-#ifdef HAVE_QUVI
-#include <quvi/quvi.h>
-#endif /* HAVE_QUVI */
-
 #include "totem-pl-parser-mini.h"
 #include "totem-pl-parser-videosite.h"
 #include "totem-pl-parser-private.h"
+
+#define BASE 20
 
 gboolean
 totem_pl_parser_is_videosite (const char *uri, gboolean debug)
 {
 #ifdef HAVE_QUVI
-	quvi_t handle;
-	QUVIcode rc;
+	const char *args[] = {
+		LIBEXECDIR "/totem-pl-parser-videosite",
+		"--check",
+		"--url",
+		NULL,
+		NULL
+	};
+	char *out;
 
-	if (quvi_init (&handle) != QUVI_OK)
-		return FALSE;
-
-	rc = quvi_supported(handle, (char *) uri);
-	quvi_close (&handle);
-
+	args[3] = uri;
+	g_spawn_sync (NULL,
+		      (char **) args,
+		      NULL,
+		      0,
+		      NULL,
+		      NULL,
+		      &out,
+		      NULL,
+		      NULL,
+		      NULL);
 	if (debug)
-		g_print ("Checking videosite for URI '%s' returned %d (%s)\n",
-			 uri, rc, (rc == QUVI_OK) ? "true" : "false");
+		g_print ("Checking videosite for URI '%s' returned '%s' (%s)\n",
+			 uri, out, g_strcmp0 (out, "TRUE") == 0 ? "true" : "false");
 
-	return (rc == QUVI_OK);
+	return (g_strcmp0 (out, "TRUE") == 0);
 #else
 	return FALSE;
 #endif /* HAVE_QUVI */
 }
 
 #ifndef TOTEM_PL_PARSER_MINI
-
-#define getprop(prop, p)					\
-	if (quvi_getprop (v, prop, &p) != QUVI_OK)		\
-		p = NULL;
 
 TotemPlParserResult
 totem_pl_parser_add_videosite (TotemPlParser *parser,
@@ -73,75 +74,64 @@ totem_pl_parser_add_videosite (TotemPlParser *parser,
 			       gpointer data)
 {
 #ifdef HAVE_QUVI
-	QUVIcode rc;
-	quvi_t handle;
-	quvi_media_t v;
+	const char *args[] = {
+		LIBEXECDIR "/totem-pl-parser-videosite",
+		"--url",
+		NULL,
+		NULL
+	};
 	char *uri;
-	/* properties */
-	const char *video_uri;
-	double length;
-	char *length_str;
-	const char *title;
-	const char *id;
-	const char *page_uri;
-	const char *starttime;
-	const char *content_type;
-	const char *thumb_url;
-	double duration;
-	char *duration_str;
-
-	if (quvi_init (&handle) != QUVI_OK)
-		return TOTEM_PL_PARSER_RESULT_ERROR;
-
-	quvi_setopt(handle, QUVIOPT_NOVERIFY, TRUE);
-	quvi_setopt(handle, QUVIOPT_FORMAT, "best");
+	char *out = NULL;
+	char **lines;
+	guint i;
+	GHashTable *ht;
+	char *new_uri = NULL;
 
 	uri = g_file_get_uri (file);
-	rc = quvi_parse(handle, uri, &v);
-	if (rc != QUVI_OK) {
-		if (totem_pl_parser_is_debugging_enabled (parser))
-			g_print ("quvi_parse for '%s' returned %d\n", uri, rc);
-		g_free (uri);
-		quvi_close (&handle);
-		if (rc == QUVI_NOSUPPORT)
+
+	args[2] = uri;
+	g_spawn_sync (NULL,
+		      (char **) args,
+		      NULL,
+		      0,
+		      NULL,
+		      NULL,
+		      &out,
+		      NULL,
+		      NULL,
+		      NULL);
+	if (totem_pl_parser_is_debugging_enabled (parser))
+		g_print ("Parsing videosite for URI '%s' returned '%s'\n", uri, out);
+
+	if (out != NULL) {
+		if (g_str_equal (out, "TOTEM_PL_PARSER_RESULT_ERROR"))
+			return TOTEM_PL_PARSER_RESULT_ERROR;
+		if (g_str_equal (out, "TOTEM_PL_PARSER_RESULT_UNHANDLED"))
 			return TOTEM_PL_PARSER_RESULT_UNHANDLED;
+	} else {
+		/* totem-pl-parser-videosite failed to launch */
 		return TOTEM_PL_PARSER_RESULT_ERROR;
 	}
 
-	getprop (QUVIPROP_MEDIAURL, video_uri);
-	if (quvi_getprop (v, QUVIPROP_MEDIACONTENTLENGTH, &length) == QUVI_OK && length)
-		length_str = g_strdup_printf ("%f", length);
-	else
-		length_str = NULL;
-	getprop (QUVIPROP_PAGETITLE, title);
-	getprop (QUVIPROP_MEDIAID, id);
-	getprop (QUVIPROP_PAGEURL, page_uri);
-	getprop (QUVIPROP_STARTTIME, starttime);
-	getprop (QUVIPROP_MEDIACONTENTTYPE, content_type);
-	getprop (QUVIPROP_MEDIATHUMBNAILURL, thumb_url);
-	if (quvi_getprop (v, QUVIPROP_MEDIADURATION, &duration) == QUVI_OK && duration)
-		duration_str = g_strdup_printf ("%f", duration);
-	else
-		duration_str = NULL;
+	ht = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, g_free);
+	lines = g_strsplit (out, "\n", -1);
+	g_free (out);
+	for (i = 0; lines[i] != NULL && *lines[i] != '\0'; i++) {
+		char **line;
 
-	if (video_uri != NULL)
-		totem_pl_parser_add_uri (parser,
-					 TOTEM_PL_PARSER_FIELD_TITLE, title,
-					 TOTEM_PL_PARSER_FIELD_ID, id,
-					 TOTEM_PL_PARSER_FIELD_MOREINFO, page_uri,
-					 TOTEM_PL_PARSER_FIELD_URI, video_uri,
-					 TOTEM_PL_PARSER_FIELD_FILESIZE, length_str,
-					 TOTEM_PL_PARSER_FIELD_STARTTIME, starttime,
-					 TOTEM_PL_PARSER_FIELD_CONTENT_TYPE, content_type,
-					 TOTEM_PL_PARSER_FIELD_IMAGE_URI, thumb_url,
-					 TOTEM_PL_PARSER_FIELD_DURATION, duration_str,
-					 NULL);
-	g_free (uri);
-	g_free (length_str);
-	g_free (duration_str);
+		line = g_strsplit (lines[i], "=", 2);
+		if (g_strcmp0 (line[0], TOTEM_PL_PARSER_FIELD_URI) == 0) {
+			if (new_uri == NULL)
+				new_uri = g_strdup (line[1]);
+		} else {
+			g_hash_table_insert (ht, g_strdup (line[0]), g_strdup (line[1]));
+		}
+		g_strfreev (line);
+	}
+	g_strfreev (lines);
 
-	quvi_parse_close (&v);
-	quvi_close (&handle);
+	totem_pl_parser_add_hash_table (parser, ht, new_uri, FALSE);
+	g_free (new_uri);
 
 	return TOTEM_PL_PARSER_RESULT_SUCCESS;
 #else
